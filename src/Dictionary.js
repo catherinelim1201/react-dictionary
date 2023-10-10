0dbda23b1459312a9015b1afe07be37e5ccd12d8
catherine lim <jkvvhappy@gmail.com>
2023-10-10 23:26:54 +0800

    added comments

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,13 +3,15 @@ import axios from "axios";
 import './Dictionary.css';
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("") /* state to store the keyword for searching*/
+  // state to store the keyword for searching
+  let [keyword, setKeyword] = useState("")
 
-  // handle the response from the Api request
+  // handle the response from the dictionary Api request
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log(response.data);
   }
 
+  // documentation - https://www.shecodes.io/learn/apis/dictionary
   // function declaration for user to search the word
   function search(event) {
     event.preventDefault();
@@ -17,17 +19,20 @@ export default function Dictionary() {
 
     const apiKey = "b7c4b4238a43dfodfa38ef20t0b7ac1c"
     const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse) /* make an api request and handle the response*/
+    // console.log(apiUrl);
+
+    // make an api request and handle the response
+    axios.get(apiUrl).then(handleResponse)
   }
 
-  // function to store the word
+  // function to store the word - handles the change in the input field for the keyword
   const handleKeywordChange = (event) => {
-    // console.log(event.target.value);
+    console.log(event.target.value);
+    // update the keywaord state when the input value changes
     setKeyword(event.target.value);
   }
 
-  // form for user
+  // render the search form for the user
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
